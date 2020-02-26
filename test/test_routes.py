43b3923bfc5e145cab0a6d8b3d31bf32cf6e0b72
Shyam Saladi <smsaladi@gmail.com>
2020-02-26 08:12:46 -0800

    add stub for tests

---
+++ test/test_routes.py
@@ -0,0 +1,14 @@
+"""
+
+A testing stub
+
+"""
+
+def test_paragraphN():
+    # "/v1/10.1101/098673v3/paragraph/28?format=html"
+    # 
+    # resolves to
+    # https://www.biorxiv.org/content/10.1101/098673v3.full
+    # 
+    """For broader consideration, one can consider the fold change in positive rate by selecting targets informed by IMProve scores. Using the data available, only testing proteins within the top 10% of scores would result in an average fold change of 2.0 in the positive rate (<em>i.e</em>. twice as many positively expressed proteins). As positive rate is a bounded quantity (maximum is 100%), the possible fold change is bounded as well and becomes relative to the overall positive rate when considering various cut-offs (<em>e.g</em>. for <em>T. maritima</em> the maximum fold-change is 15.4 while for archaeal transporters it is 3.3). Taking the average maximum possible fold change (7.5), the IMProve model achieves nearly a third of the possible improvement in positive rate compared to a perfect predictor."""
+    assert False
